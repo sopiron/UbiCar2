2eeb608b1a8a276e30ef366b28d118f58e2896e6
sopiron <167652553+sopiron@users.noreply.github.com>
2026-04-14 12:44:00 -0300

    version3

--- UbiCar-main/demo/src/main/java/com/uade/tpo/demo/controllers/productImage/ProductImageController.java
+++ UbiCar-main/demo/src/main/java/com/uade/tpo/demo/controllers/productImage/ProductImageController.java
@@ -2,12 +2,20 @@ package com.uade.tpo.demo.controllers.productImage;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import com.uade.tpo.demo.service.productImage.ProductImageService;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -15,29 +23,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/product-images")
 public class ProductImageController {
     
+    @Autowired
+    private ProductImageService productImageService;
 
-     @GetMapping // Obtenemos las imagenes de un producto
-    public String getProductImage(@RequestParam String productId) {
-        return new String();
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getImage(@PathVariable Long id) {
+        String base64 = productImageService.getImage(id);
+    
+        return ResponseEntity.ok(base64);
     }
 
-    @GetMapping ("/{productId}")// Obtenemos la imagen del producto por el id
-    public String getProductImageById(@PathVariable String productId) {
-        return new String();
-    }
 
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file, @RequestParam Long productId) {
 
-    @PostMapping // Creamos una nueva imagen para un producto
-    public String createImage(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+        try {
+            productImageService.uploadImage(file, productId);
+            return ResponseEntity.ok("Imagen subida correctamente");
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error al subir imagen");
+        }
     }
     
 
-    @DeleteMapping("/{productId}") // Eliminamos la imagen de un producto por el id
-    public void deleteProductImage(@PathVariable String productId) {
-        
+   @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteImage(@PathVariable Long id) {
+
+        productImageService.deleteImage(id);
+        return ResponseEntity.ok("Imagen eliminada correctamente");
     }
 
     
