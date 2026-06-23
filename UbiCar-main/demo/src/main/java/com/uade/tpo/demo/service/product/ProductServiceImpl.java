d1b75b638e385b63a0c2fd6f7c4c42e7296f0a04
sopiron <167652553+sopiron@users.noreply.github.com>
2026-06-23 00:58:47 -0300

    no cambie nada

--- UbiCar-main/demo/src/main/java/com/uade/tpo/demo/service/product/ProductServiceImpl.java
+++ UbiCar-main/demo/src/main/java/com/uade/tpo/demo/service/product/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.uade.tpo.demo.controllers.product.ProductDeleteRequest;
